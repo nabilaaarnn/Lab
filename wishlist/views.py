122e8a4396ce03fc17ec78f2de08217718c93107
Your Name <rizka.nisrina@ui.ac.id>
2022-10-06 16:33:46 +0700

    Lab 5

--- wishlist/views.py
+++ wishlist/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
@@ -67,3 +68,7 @@ def show_json(request):
 def show_json_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_wishlist_ajax(request):
+    context = {}
+    return render(request, "wishlist_ajax.html", context)
